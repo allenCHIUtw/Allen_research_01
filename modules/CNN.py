4b4424217d4fa751b1f1f1bddee6cc792aecadcb
allenCHIUtw <allen@abin.com.tw>
2026-06-21 15:13:59 +0800

    621noon

---
+++ modules/CNN.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MotionDbNN(nn.modules):
+    def __init__(self):
+        super(MotionDbNN , self).__init__()
+        self.conv
