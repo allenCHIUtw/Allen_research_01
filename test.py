1dede70c6debcc238c05ca1d3a362db600f17c56
allenCHIUtw <allen@abin.com.tw>
2026-06-24 02:22:40 +0800

    0623_v2

--- test.py
+++ test.py
@@ -10,11 +10,22 @@ from modules.evalways import compute_niqe_score
 parser = argparse.ArgumentParser()
 parser.add_argument("--p", "-path", help="the path of data",type=str,dest="path")
 parser.add_argument("--m","-mode", help="the path of data",type=str)
+parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        required=True,
+        help="Path to the CSV data file",
+    )
 args = parser.parse_args()
 # PATH = "/media/allenx570/資料磁碟/disp_data/AGZ_subset/MAV Images/00114.jpg"
 
 PATH = args.path
+FILEPATH = args.file
 IMG_01 = cv2.imread(PATH)
+csv_1 = pd.read_csv(FILEPATH)
+
+
 
 
 # dispatch = {
@@ -25,7 +36,9 @@ IMG_01 = cv2.imread(PATH)
 
 # # 3. Execute the selected function
 # dispatch[args.mode]()
+def main():
 
 
-
+if __name__ == "__main__":
+    main()
 
