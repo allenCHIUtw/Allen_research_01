4723df0107cbf4881ed4721f1510212cc889f259
allenCHIUtw <allen@abin.com.tw>
2026-06-30 02:58:19 +0800

    629

--- modules/deblurM1.py
+++ modules/deblurM1.py
@@ -8,12 +8,13 @@ from scipy.fftpack import fft, fftfreq
 def vb_blurr_jello(img, vibration_hz, amplitude_pixels, row_readout_time=1e-5):  # vibration blurr
 
 def vb_blurr_harmonic(img, frequency, amplitude, direction_angle, exposure_time=1/500):
-    
+    # need to do 
 
 def rotl_blurr_6dof(img, omega_3d, exposure_time, intrinsic_matrix): # rotation movement blurr
+   # need intrincix matrix
 
-
-def lin_blurr_spatial_variant(img, depth_map, velocity_3d, intrinsic_matrix)#linear movement blurr must 6 dof
+def lin_blurr_spatial_variant(img, depth_map, velocity_3d ):#linear movement blurr must 6 dof
+  # velocity do not means m/s just means the speed of the pixel movemet for generalization
 
 
  
